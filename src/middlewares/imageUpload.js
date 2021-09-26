35ae5d6052520b0f0c01414e0964e9b74ee39e86
Mirailisc <mirailisclm@gmail.com>
2021-09-27 05:44:11 +0700

    fix image upload

--- src/middlewares/imageUpload.js
+++ src/middlewares/imageUpload.js
@@ -1,9 +1,10 @@
 import multer from 'multer'
 import path from 'path'
+const __dirname = path.resolve();
 
 const storage = multer.diskStorage({
     destination: (req, file, callBack) => {
-        callBack(null, process.cwd() + '/public/')     // './public/images/' directory name where save the file
+        callBack(null, __dirname + '/public/')     // './public/images/' directory name where save the file
     },
     filename: (req, file, callBack) => {
         callBack(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname))
